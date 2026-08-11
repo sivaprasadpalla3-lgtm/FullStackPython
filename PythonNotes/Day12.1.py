9f6c11b21d601f0a4092686f8b46c566c2ec85c3
Siva Prasad Reddy <sivaprasadpalla3@gmail.com>
2026-08-11 21:09:49 +0530

    Day 12 of python notes

---
+++ PythonNotes/Day12.1.py
@@ -0,0 +1,31 @@
+# Basic Level Questions
+'''
+a = int(input())
+
+if a >= 0:
+    print("Positive")
+else:
+    print("Negative")'''
+
+'''
+age=int(input())
+
+if age>=18:
+    print("You are eligible to Vote")
+else:
+    print("Not eligible")'''
+
+'''a=int(input())
+b=int(input())
+
+if a>b:
+    print(f"{a}")
+else:
+    print()'''
+
+a=input()
+
+if a.isalpha():
+    print("Letter")
+else:
+    print("Not Letter")
